fe50ce99f99051f6c33dbeb5553eccb208dce916
Mike Lin <95307563+Maiku3@users.noreply.github.com>
2023-12-03 21:31:23 -0500

    Implemented note retrieval from db

---
+++ src/app/notebook/[noteId]/page.tsx
@@ -0,0 +1,39 @@
+import { db } from '@/lib/db'
+import { $notes } from '@/lib/db/schema'
+import { auth } from '@clerk/nextjs'
+import { and, eq } from 'drizzle-orm'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+type Props = {
+    params : {
+        noteId: string
+    }
+}
+
+// Server component for the notebook page
+const NotebookPage = async ({params: {noteId}}: Props) => {
+    const {userId} = await auth();
+    if (!userId) {
+        return redirect(('/dashboard'))
+    }
+
+    // Fetch the note from the database
+    const notes = await db.select().from($notes).where(and(eq($notes.id, parseInt(noteId)),eq($notes.userId, userId)))
+
+    if (notes.length === 0) {
+        return redirect('/dashboard')
+    }
+    const note = notes[0]
+
+  return (
+    <div className="min-h-screen p-10">
+        <div className="max-w-4xl mx-auto">
+        </div>
+
+    </div>>
+  )
+}
+
+export default NotebookPage
