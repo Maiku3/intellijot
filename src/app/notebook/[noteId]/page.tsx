d5d1e0fb2acd765d4cd2ded8e86cb33bc1e1a6c2
Mike Lin <95307563+Maiku3@users.noreply.github.com>
2023-12-06 12:19:12 -0500

    Added Back Button to Notebook page

--- src/app/notebook/[noteId]/page.tsx
+++ src/app/notebook/[noteId]/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import { db } from '@/lib/db'
 import { $notes } from '@/lib/db/schema'
 import { auth } from '@clerk/nextjs'
@@ -16,28 +17,28 @@ type Props = {
 const NotebookPage = async ({params: {noteId}}: Props) => {
     const {userId} = await auth();
     if (!userId) {
-        return redirect(('/dashboard'))
+        return redirect(('/dashboard'));
     }
 
     // Fetch the note from the database
     const notes = await db.select().from($notes).where(and(eq($notes.id, parseInt(noteId)),eq($notes.userId, userId)))
 
     if (notes.length === 0) {
-        return redirect('/dashboard')
+        return redirect('/dashboard');
     }
-    const note = notes[0]
+    const note = notes[0];
 
   return (
     <div className="min-h-screen p-10">
         <div className="max-w-4xl mx-auto">
             <div className='border shadow-xl border-stone-200 rounded-lg p-4 flex items-center'>
             <Link href={'/dashboard'}>
+                <Button className='bg-blue-600'size='sm'>Back</Button>
             </Link>
             </div>
         </div>
-
-    </div>>
-  )
-}
+    </div>
+  );
+};
 
 export default NotebookPage
