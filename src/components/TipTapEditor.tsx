ae78b20d592c23db56ad5095dd9409dcaa65eabc
Mike Lin <95307563+Maiku3@users.noreply.github.com>
2023-12-09 16:19:14 -0500

    Added saveNote endpoint and function

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -5,23 +5,47 @@ import {StarterKit} from '@tiptap/starter-kit'
 import TipTapToolbar from './TipTapToolbar';
 import { Button } from './ui/button';
 import { useDebounce } from '@/lib/useDebounce';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { NoteType } from '@/lib/db/schema';
 
-type Props = {}
+type Props = {note: NoteType}
 
-const TipTapEditor = (props: Props) => {
-    const [editorState, setEditorState] = React.useState('')
-
+const TipTapEditor = ({ note }: Props) => {
+    const [editorState, setEditorState] = React.useState('');
+    const saveNote = useMutation({
+        mutationFn: async() => {
+            const response = await axios.post('/api/saveNote', {noteId: note.id, editorState});
+            return response.data;
+        }
+    
+    })
     const editor = useEditor({
         autofocus: true,
         extensions: [StarterKit],
         content: editorState,
         onUpdate: ({editor}) => {
-            setEditorState(editor.getHTML())
+            setEditorState(editor.getHTML());
         },
     });
 
     const debouncedEditorState = useDebounce(editorState, 500);
-
+    React.useEffect(() => {
+        // Save Note to DB
+        if (debouncedEditorState === '') {
+            return;
+        }
+        saveNote.mutate(undefined, {
+            onSuccess: (data) => {
+                console.log('data', data);
+            },
+            onError: (error) => {
+                console.log('error', error);
+            }
+        
+        });
+        console.log('debouncedEditorState', debouncedEditorState);
+    }, [debouncedEditorState]);
   return (
     <>
         <div className="flex">
